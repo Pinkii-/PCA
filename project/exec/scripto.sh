0641130c326f977727d40d4daa4661d11991b0ae
pnkthrepwood <pnkthrepwood@gmail.com>
2016-05-01 23:39:22 +0200

    el escript bueno

--- project/exec/scripto.sh
+++ project/exec/scripto.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-echo "PRIMER TESTO!"
-../sources/3D_Dock/progs/ftdock -static ../inputs/2pka.parsed -mobile ../inputs/5pti.parsed > output.test1
+echo "PRIMER TEST!"
+../sources/3D_Dock_2/progs/ftdock -static ../inputs/2pka.parsed -mobile ../inputs/5pti.parsed > output.test1
 
-echo "SECOND TESTO!"
+echo "SEGON TEST!"
 ../sources/3D_Dock/progs/ftdock -static ../inputs/1hba.parsed -mobile ../inputs/5pti.parsed > output.test2
 
-echo "TERCER TESTO!"
+echo "TERCER TEST!"
 ../sources/3D_Dock/progs/ftdock -static ../inputs/4hhb.parsed -mobile ../inputs/5pti.parsed > output.test3
 
-echo "ULTIM TESTO!"
+echo "ULTIM TEST!"
 ../sources/3D_Dock/progs/ftdock -static ../inputs/1ACB_rec.parsed -mobile ../inputs/1ACB_lig.parsed > output.test4
